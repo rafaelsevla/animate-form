eb795198494274fbd06687a445f003c655bcab10
Rafael Costa <rafaelsevla@gmail.com>
2019-09-13 21:15:07 -0300

    hidden form when click on button

--- base/script.js
+++ base/script.js
@@ -5,3 +5,15 @@ btnLogin.addEventListener('click', event => {
   event.preventDefault()
   form.classList.add('form-hide')
 })
+
+form.addEventListener('animationstart', event => {
+  if (event.animationName === 'down')
+    document.querySelector('body').style.overflow = 'hidden'
+})
+
+form.addEventListener('animationend', event => {
+  if (event.animationName === 'down') {
+    form.style.display = 'none'
+    document.querySelector('body').style.overflow = 'none'
+  }
+})
