564188b2e0bd895e3181853f7b4478209e8f51ff
Rafael Costa <rafaelsevla@gmail.com>
2019-09-13 21:37:35 -0300

    add nono animation

--- base/script.js
+++ base/script.js
@@ -3,12 +3,29 @@ const form = document.querySelector('form')
 
 btnLogin.addEventListener('click', event => {
   event.preventDefault()
-  form.classList.add('form-hide')
+
+  const fields = [...document.querySelectorAll('.input-block input')]
+
+  fields.forEach(field => {
+    if (field.value === '') form.classList.add('validate-error')
+  })
+
+  const formError = document.querySelector('.validate-error')
+  if (formError) {
+    formError.addEventListener('animationend', event => {
+      if (event.animationName === 'nono') {
+        formError.classList.remove('validate-error')
+      }
+    })
+  } else {
+    form.classList.add('form-hide')
+  }
 })
 
 form.addEventListener('animationstart', event => {
-  if (event.animationName === 'down')
+  if (event.animationName === 'down') {
     document.querySelector('body').style.overflow = 'hidden'
+  }
 })
 
 form.addEventListener('animationend', event => {
